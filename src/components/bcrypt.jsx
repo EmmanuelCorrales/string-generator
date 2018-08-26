87885bd5ff016b79828a6d29977de92a46400d72
Emmanuel Corrales <contact@emmanuelcorrales.com>
2018-08-26 20:48:02 +0800

    Create inital implementation of home and bcrypt.

---
+++ src/components/bcrypt.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const Bcrypt = () => (<h1>Bcrypt tools coming soon.</h1>);
+
+export default Bcrypt;
---
+++ src/components/home.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const Home = () => (<h1>Welcome to my Web Tools!</h1>);
+
+export default Home;
