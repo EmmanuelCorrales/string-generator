8da3c9c792cf0713ce0e6c152771e4284a897bd1
Emmanuel Corrales <contact@emmanuelcorrales.com>
2018-08-26 23:47:48 +0800

    Show a swipeable drawer after clicking the hamburger icon.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 class NavBar extends Component {
+
+  state = {
+    isDrawerOpen: false
+  };
+
+  toggleDrawer = () => {
+    this.setState({ isDrawerOpen: !this.state.isDrawerOpen  });
+  }
 
   render() {
     return (
       <div>
         <AppBar position="static">
           <Toolbar>
-            <IconButton  color="inherit" aria-label="Menu">
+            <IconButton color="inherit" aria-label="Menu"
+              onClick={ this.toggleDrawer } >
               <MenuIcon />
             </IconButton>
             <Typography variant="title" color="inherit" >
@@ -20,6 +33,22 @@ class NavBar extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
+        <SwipeableDrawer
+          open={ this.state.isDrawerOpen }
+          onOpen={ this.toggleDrawer }
+          onClose={ this.toggleDrawer } >
+          <List component="nav">
+            <ListItem button>
+              <ListItemText primary="Home" href="/" />
+            </ListItem>
+            <ListItem button component="a" href="/string-generator">
+              <ListItemText primary="String Generator" />
+            </ListItem>
+            <ListItem button component="a" href="/bcrypt">
+              <ListItemText primary="Bcrypt" />
+            </ListItem>
+          </List>
+        </SwipeableDrawer>
       </div>
     );
   }
