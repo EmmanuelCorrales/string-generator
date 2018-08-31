9feca4ac00308b9b89ba62f3b3a40baf9d2e8732
Emmanuel Corrales <contact@emmanuelcorrales.com>
2018-08-31 10:01:03 +0800

    Add UI for decryption.

--- src/components/Bcrypt/bcrypt.jsx
+++ src/components/Bcrypt/bcrypt.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import bcrypt from 'bcryptjs';
 
 import './bcrypt.css';
@@ -25,24 +26,43 @@ class Bcrypt extends Component {
     const { textToHash, hashedText } = this.state;
 
     return (
-      <div>
+      <div className='bcrypt'>
         <br/>
-        <div>
-          <TextField placeholder='String to encrypt.'
-            onChange={ this.onChangeTextToHash }
-            value={ textToHash } />
-          &nbsp;
-          <Button variant='contained'
-            onClick={ this.hashText } >
-            Encrypt
-          </Button>
-        </div>
-        <br/>
-        <div>
+        <div className='item'>
+          <div>
+            <Typography variant='headline'>Encrypt</Typography>
+            <br/>
+            <TextField placeholder='String to encrypt.'
+              onChange={ this.onChangeTextToHash }
+              value={ textToHash } />
+            &nbsp;
+            <Button variant='contained'
+              onClick={ this.hashText } >
+              Encrypt
+            </Button>
+          </div>
+          <br/>
           <TextField multiline
             placeholder='Encrypted string.'
             rowsMax={ 12 }
             value={ hashedText } />
+        </div>
+        <div className='item'>
+          <Typography variant='headline'>Decrypt</Typography>
+          <br/>
+          <TextField placeholder='Hash to check'
+            value={ textToHash } />
+          <br/>
+          <br/>
+          <div>
+            <TextField
+              placeholder='String to check'
+              rowsMax={ 12 } />
+            &nbsp;
+            <Button variant='contained' >
+              Encrypt
+            </Button>
+          </div>
         </div>
       </div>
     );
